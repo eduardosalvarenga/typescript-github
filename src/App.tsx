1a808c42cf1855faec96f0eb2da9688e78e56b10
eduardosalvarenga <eduardosalvarenga@gmail.com>
2020-12-27 21:29:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import NavBar from './components/Search/Search';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Search />
     </div>
   );
 }
